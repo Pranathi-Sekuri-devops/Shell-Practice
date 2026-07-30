0c2b67c8d8446a3d1bc31e5aecc9d8d9cf0d6a75
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-07-30 21:37:25 +0530

    malar & ram's chat

---
+++ 02.conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Malar: I recently learned how to write and run shell scripts."
+echo "Ram: That's great! I also learned how to use variables and echo commands."
+echo "Malar: These basics make automation feel much easier."
+echo "Ram: Yes, and I am excited to learn more about shell scripting!"
