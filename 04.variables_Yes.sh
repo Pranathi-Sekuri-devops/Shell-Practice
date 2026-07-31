ca2e4e9ea1e827d4a59ae19b723c3ee7b5ee486c
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-07-31 11:54:50 +0530

    with variables

--- 04.variables_Yes.sh
+++ 04.variables_Yes.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 # with variables
+
+# variables:
+#------------
 # variables are used to store values that can be reused throughout the script, making it easier to 
 # maintain and update the script.
+
+# advantages of using variables:
+#---------------------------------
+# 1. Reusability: Variables allow you to store values that can be reused throughout the script, reducing redundancy and making the code more concise.
+# 2. Maintainability: If you need to change a value, you can simply update the variable instead of searching and replacing it in multiple places in the script, making it easier to maintain and update the code.
+# 3. Readability: Using descriptive variable names can make the code more readable and easier to understand, especially for complex scripts with many values and operations.
+# 4. Flexibility: Variables can be assigned different values at runtime, allowing for more dynamic and flexible scripts that can adapt to different inputs and conditions.  
+
 # syntax: variable_name="value" 
 # can be accessed using the $ symbol.
  
