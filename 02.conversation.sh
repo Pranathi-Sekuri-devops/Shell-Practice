ed9fefdd732ba96f1e2412428d86944f78c3f4ca
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-07-31 12:04:48 +0530

    ram & malar's chat

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This script demonstrates a simple conversation between two people using echo commands in a shell script.
+
 echo "Malar: I recently learned how to write and run shell scripts."
 echo "Ram: That's great! I also learned how to use variables and echo commands."
 echo "Malar: These basics make automation feel much easier."
