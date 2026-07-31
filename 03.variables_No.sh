2aae242b5e45dbf459cc8fda7459d530529b03b1
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-07-31 11:54:23 +0530

    without variables

--- 03.variables_No.sh
+++ 03.variables_No.sh
@@ -9,3 +9,4 @@ echo "Ram: Yes, Malar, I am excited to learn more about shell scripting with you
 
 # names are repeated and if we want to change the names we have to change it in multiple places which is
 # not efficient.
+# thats why we use variables to store values that can be reused throughout the script.
