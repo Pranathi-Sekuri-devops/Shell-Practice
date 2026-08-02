607083e60656e49e94e9207aa889866941ca8626
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-02 12:15:38 +0530

    reading-variables

--- 06.variables_read.sh
+++ 06.variables_read.sh
@@ -16,3 +16,7 @@ echo "Please enter the second name: "
 read name2
 
 echo "$name1, $name2"
+
+# for eg : if my runtime command is : bash 06.variables_read.sh, then
+# the script will prompt the user to enter the first name and second name, and then it will output the 
+#  entered names in the format: "first_name, second_name".
