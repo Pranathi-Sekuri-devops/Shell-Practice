2127f92084b1aadd956147c8e35d62af9075f8ba
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-03 20:43:08 +0530

    command substitution in variables

---
+++ 09.variables_cmnd_subst.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# command substitution
+#--------------------
+# Command substitution allows us to capture the output of a command and use it as a variable or
+# within another command.
+# This way, we can dynamically generate values based on the results of commands, making our scripts
+# more flexible and powerful.
+
+# syntax: variable_name=$(command)
+# can be accessed using the $ symbol.
+
+Date=$(date)
+echo "The current date and time is: $Date"
+
+# here date command executes in linux server and the output of the command is stored in the variable Date,
+# which is then printed to the console using the echo command.
+
+start_time=$(date +%s)
+sleep 10
+end_time=$(date +%s)
+execution_time=$((end_time-start_time))
+# the above code calculates the execution time of a script by capturing the start and end times in seconds
+# The difference between the end time and start time 
+# is then calculated and stored in the execution_time variable, which can be used for performance analysis
+# or optimization purposes.
