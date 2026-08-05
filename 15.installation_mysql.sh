3924ae37c00c8cb78f2c389407efc177d4e30996
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-05 18:43:13 +0530

    mysql installation with script

--- 15.installation_mysql.sh
+++ 15.installation_mysql.sh
@@ -6,15 +6,13 @@
 user=$(id -u)
 
 if [$user -ne 0]; then
-    echo " user should have root previleges "
+    echo " ERROR : user should have root previleges "
     exit 1 # stopping the script to run further
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0]; then
-    echo " mysql installation failed "
+    echo " ERROR : mysql installation failed "
 else 
     echo " mysql is successfully installed ! "
-
- 
