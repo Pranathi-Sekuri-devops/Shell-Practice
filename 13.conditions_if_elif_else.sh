b76c7e4536c18bad99e3620d01412a2638d0363f
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-05 09:29:46 +0530

    conditions_if-else

---
+++ 13.conditions_if_elif_else.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if-elif-else
+#-------------------
+# We can use if-elif-else statements to execute different blocks of code depending on multiple conditions.
+
+# syntax:
+# if [ condition1 ]; then
+#     # code to execute if condition1 is true
+# elif [ condition2 ]; then
+#     # code to execute if condition1 is false and condition2 is true
+# else
+#     # code to execute if none of the above conditions are true
+# fi
+
+num=$1
+if [ $num -gt 10 ]; then
+    echo "$num is greater than 10"
+elif [ $num -eq 10 ]; then
+    echo "$num is equal to 10"
+else
+    echo "$num is less than 10"
+fi
