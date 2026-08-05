f3e44d18abc68ee8381d3757ab532df3d1417942
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-05 10:52:00 +0530

    even or odd

--- 14.even_or_odd.sh
+++ 14.even_or_odd.sh
@@ -13,5 +13,4 @@ fi
 
 # The modulo operator (%) is used to find the remainder when the number is divided by 2. 
 # If the remainder is 0, the number is even; otherwise, it is odd.
-# For example, if the input number is 4, the output will be "4 is an even no".
-# If the input number is 7, the output will be "7 is an odd no".
+
