1fa593bda9a956be3ed8aad1a73e89d43312c6a4
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-05 11:56:37 +0530

    even or odd

--- 14.even_or_odd.sh
+++ 14.even_or_odd.sh
@@ -4,13 +4,14 @@
 #----------------------
 # This script checks whether a given number is even or odd.
 
-num=$1
-if [ $((num%2)) -eq 0 ]; then
-    echo "$num is an even no"
+read -p "Enter a number: " num
+if [ $(($num%2)) -eq 0 ]; then
+    echo "$num is an even number"
 else
-    echo "$num is an odd no"
+    echo "$num is an odd number"
 fi
 
+# -p : Used with the read command to prompt the user for input.
 # The modulo operator (%) is used to find the remainder when the number is divided by 2. 
 # If the remainder is 0, the number is even; otherwise, it is odd.
 
