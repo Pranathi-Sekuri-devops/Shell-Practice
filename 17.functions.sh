73a40da959db19d31ec0337fe09475c4fe44061e
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-06 12:04:15 +0530

    installing multiple packages using a function

--- 17.functions.sh
+++ 17.functions.sh
@@ -6,6 +6,41 @@
 # version -2
 # -----------
 
+# user=$(id -u)
+
+# if [ $user -ne 0 ]; then
+#     echo " Error : user should have root previliges "
+#     exit 1
+# fi
+
+# # functions wont execute on its own. only when it is called, it will run.
+# validate() 
+# {
+#     if [ $1 -ne 0 ]; then
+#         echo " Error : Failed to install $2 "
+#     else
+#         echo " $2 is successfully installed ! "
+#     fi
+# }
+
+# dnf install mysql -y
+# validate $? "MySQL" # functions receive inputs like args, store in $1, $2 etc just like shell script args
+
+# dnf install nginx -y
+# validate $? "Nginx"
+
+# dnf install mongodb-mongosh -y
+# validate $? "mongodb-mongosh" # not configured its repo, so lets keep this aside as of now
+
+# dnf install python-3 -y
+# validate $? "python-3" # python-3 is typo , python3 is correct
+
+# almost 10 lines of code got reduced now. 
+# readability and reusability is achieved. performance is same.
+
+# version -2
+# -----------
+
 user=$(id -u)
 
 if [ $user -ne 0 ]; then
@@ -24,17 +59,10 @@ validate()
 }
 
 dnf install mysql -y
-validate $? "MySQL" # functions receive inputs like args, store in $1, $2 etc just like shell script args
+validate $? "MySQL" 
 
 dnf install nginx -y
 validate $? "Nginx"
 
-dnf install mongodb-mongosh -y
-validate $? "mongodb-mongosh"
-
-dnf install python-3 -y
-validate $? "python-3"
-
-# almost 10 lines of code got reduced now. 
-# readability and reusability is achieved. performance is same.
-
+dnf install python3 -y
+validate $? "python3" # corrected
