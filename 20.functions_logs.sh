4d79bcf88cc2b0eb6042e694028e7b09a6d04321
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-06 21:20:44 +0530

    functions & color codes

--- 20.functions_logs.sh
+++ 20.functions_logs.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# version - 4
+# version - 5
 # ------------
-# my own thought of keeping validate() function inside check(). well done malar !
+# logging results in log files.
 
 user=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 W="\e[0m"
+logs_folder= "/var/log/shellscript"
+script_name= $(echo $0 | cut -d "." -f1) 
+log_file="$logs_folder/$script_name.log"
+
+mkdir -p $logs_folder # -p will check if dir already exists, else it creates. without -p if dir already exists, it throws an error.
+
+echo "script execution started at : $(date)"
 
 if [ $user -ne 0 ]; then
     echo -e " $R Error : user should have root previliges $W"
@@ -19,7 +26,7 @@ check()
 {
     if [ $1 -ne 0 ]; then
         echo -e " $G Installing $2 ... $W"
-        dnf install $2 -y
+        dnf install $2 -y &>>$log_file
         validate $? $2
     else
         echo -e " $Y $2 is already installed ! $W"
@@ -35,12 +42,11 @@ validate()
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql &>>$log_file
 check $? "mysql" 
 
-dnf list installed nginx
+dnf list installed nginx &>>$log_file
 check $? "nginx" 
 
-dnf list installed python3
+dnf list installed python3 &>>$log_file
 check $? "python3" 
-
