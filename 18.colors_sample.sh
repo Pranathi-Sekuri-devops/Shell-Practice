26337d2be2c99d89fb1ac40ef89950417ef85aa5
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-06 12:55:17 +0530

    color codes in shell script

--- 18.colors_sample.sh
+++ 18.colors_sample.sh
@@ -2,10 +2,22 @@
 
 # color code in shell
 #--------------------
+# version -1
+# ------------
+# r="\e[31m"
+# g="\e[32m"
+# y="\e[33m"
 
+# echo -e "$r hello world" 
+# echo " this is red color" # the color passes on to the next echos as well
+
+# version -2
+# ------------
 r="\e[31m"
 g="\e[32m"
 y="\e[33m"
+n="\e[0m"
+echo -e "$r hello world $n" # hello world in red color and color changes to normal color
+echo " this is red color"  # in white color.
 
-echo -e "$r hello world"
-echo " this is red color"
+
