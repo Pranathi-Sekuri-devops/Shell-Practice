42042399edd04a672176155d036465d3f5471a55
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-06 22:05:43 +0530

    functions & logs

--- 20_functions_logs.sh
+++ 20_functions_logs.sh
@@ -15,7 +15,7 @@ script_name=$(echo $0 | cut -d "." -f1) # removing .sh part from the filename 20
 log_file="$logs_folder/$script_name.log"
 
 mkdir -p $logs_folder  # -p will check if dir already exists, else it creates. without -p if dir already exists, it throws an error.
-echo "script execution started at : $(date)"
+echo "script execution started at : $(date)" | tee -a $log_file
 
 if [ $user -ne 0 ]; then
     echo -e " $R Error : user should have root previliges $W"
@@ -25,20 +25,20 @@ fi
 check() 
 {
     if [ $1 -ne 0 ]; then
-        echo -e " $G Installing $2 ... $W"
+        echo -e " $G Installing $2 ... $W" | tee -a $log_file
         dnf install $2 -y &>> $log_file
         validate $? $2
     else
-        echo -e " $Y $2 is already installed ! $W"
+        echo -e " $Y $2 is already installed ! $W" | tee -a $log_file
     fi
 }
 
 validate() 
 {
     if [ $1 -ne 0 ]; then
-        echo -e " $R Error : Failed to install $2 $W"
+        echo -e " $R Error : Failed to install $2 $W" | tee -a $log_file
     else
-        echo -e " $G $2 is successfully installed ! $W "
+        echo -e " $G $2 is successfully installed ! $W " | tee -a $log_file
     fi
 }
 
