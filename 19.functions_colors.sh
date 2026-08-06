e6a5c30757a3d9a9418b8dc9361ba31184c554f7
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-06 14:17:21 +0530

    functions & color codes

--- 19.functions_colors.sh
+++ 19.functions_colors.sh
@@ -7,18 +7,18 @@ Y="\e[33m"
 W="\e[0m"
 
 if [ $user -ne 0 ]; then
-    echo " $R Error : user should have root previliges $W"
+    echo -e " $R Error : user should have root previliges $W"
     exit 1
 fi
 
 check() 
 {
     if [ $1 -ne 0 ]; then
-        echo " $G Installing $2 ... $W"
+        echo -e " $G Installing $2 ... $W"
         dnf install $2 -y
         validate $? $2
     else
-        echo " $Y $2 is already installed ! $W"
+        echo -e " $Y $2 is already installed ! $W"
         exit 1
     fi
 }
@@ -26,9 +26,9 @@ check()
 validate() 
 {
     if [ $1 -ne 0 ]; then
-        echo " $R Error : Failed to install $2 $W"
+        echo -e " $R Error : Failed to install $2 $W"
     else
-        echo " $G $2 is successfully installed ! $W "
+        echo -e " $G $2 is successfully installed ! $W "
     fi
 }
 
